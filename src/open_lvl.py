563e05789d74f5260c0efcf1451fd20b2ec64424
diamondblock935 <diamondblock110@gmail.com>
2026-04-07 08:39:51 +0200

    Remove unused imports from open_lvl.py

--- src/open_lvl.py
+++ src/open_lvl.py
@@ -1,6 +1,4 @@
 import json
-from objects import Circle, slider, Approach_Circle
-# from main import Circle, slider, menu_button, Approach_Circle
 
 def load_level(file_path):
     with open(file_path, 'r') as file:
