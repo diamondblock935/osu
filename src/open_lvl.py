58e93b31191a14d171ab7408e1d9e2fffabd2f66
diamondblock935 <diamondblock110@gmail.com>
2026-06-04 10:05:08 +0200

    Update open_lvl.py

--- src/open_lvl.py
+++ src/open_lvl.py
@@ -1,7 +1,8 @@
 import json
+from os import path
 
 def load_level(file_path):
-    with open("levels/" + file_path, 'r') as file:
+    with open(path.dirname(__file__) + "/levels/" + file_path, 'r') as file:
         level_data = json.load(file)
     return level_data
     
